b0515326c337e7d88d30f409f85978729433fa89
marina_sakovich <sakovichm@mail.ru>
2017-02-05 17:11:03 +0300

    task was done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,48 @@
+
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.foundNumber = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
+    /**
+     * method accepts min and max value of range of number to guess
+     * @param min
+     * @param max
+     */
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+    /**
+     * method returns solution candidate (you make an assumption based on range and previous assumptions)
+     */
     guess() {
-
+        this.foundNumber = Math.round( (this.min + (this.max - this.min) / 2) );
+        return this.foundNumber;
     }
 
+    /**
+     * this method is called if prev call of `guess()` returned number which is greater than answer
+     */
     lower() {
+        this.setRange(this.min, this.foundNumber);
 
     }
 
+    /**
+     * method is called if prev call of `guess()` returned number which is lower than answer
+     */
     greater() {
+        this.setRange(this.foundNumber, this.max);
 
     }
 }
 
+
+
 module.exports = GuessingGame;
+
